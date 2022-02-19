cb1239222cabfe8ab6bbcd2141608a5014a078e0
Tanvi <tanishaarora1110@gmail.com>
2022-02-18 21:30:49 -0800

    modified

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <AddContact />
+      {/* <ContactList /> */}
+
+
+    </div>
+  );
+}
+
+export default App;
